c078c78becfc720479a40d9c9622290f268b60bc
xzc9607 <297243970@qq.com>
2019-01-16 00:26:37 +0800

    sdf

--- app/pages/login/register/registerpage.js
+++ app/pages/login/register/registerpage.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
 import { Text, View, TextInput, Dimensions } from 'react-native';
 import Button from 'react-native-button';
+import Global from '../../Global'
 
 const { width } = Dimensions.get('window')//获取当前屏幕宽度
 
 
 export default class Register extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = { 
+            username: '' ,
+            password:''
+
+        };
+    }
     static navigationOptions = {
         title: '注册',
     };
+
+    register() {
+        let formData = {
+          "username":this.state.username,
+          "password":this.state.password
+        }
+      
+        fetch(gUrl.url+'/register',
+        {
+           method:"POST",   //请求方法
+           mode: "cors",
+           body:JSON.stringify(formData),   //请求体
+      　　　　headers: {
+      　　　　'Accept': 'application/json',
+      　　　　'Content-Type': 'application/json',
+      　　　　 }})
+            .then((response) => {
+              res=JSON.parse(response._bodyText)
+              if(res.code==200){
+                this.props.navigation.navigate('Login');
+              }else{
+                  alert(res.msg)
+              }
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+      }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: 'white' , alignItems: 'center' }}>
                 <View style={{ width:width-40}}>
                     <TextInput
                         placeholder='手机号'
+                        onChangeText={(username) => this.setState({username})}
                     ></TextInput>
                     <View style={{ height: 1, backgroundColor: 'gray' }}></View>
                 </View>
                 <View style={{ width:width-40}}>
                     <TextInput
                         placeholder='设置密码'
+                        onChangeText={(password) => this.setState({password})}
                     ></TextInput>
                     <View style={{ height: 1, backgroundColor: 'gray' }}></View>
                 </View>
@@ -45,7 +84,7 @@ export default class Register extends Component {
                     style={{ fontSize: 20, color: 'white' }}
                     styleDisabled={{ color: 'red' }}
                     containerStyle={{ padding: 5, height: 40, width:width-40, marginTop: 50, overflow: 'hidden', borderRadius: 12, backgroundColor: 'red' }}
-                    onPress={() => this.props.navigation.navigate('Main')}>
+                    onPress={() => this.register()}>
                     注册
                 </Button>
 
