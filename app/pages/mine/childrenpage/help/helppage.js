04a703a7db97b3d0681779b456ba37f7c8a43eda
xzc9607 <297243970@qq.com>
2019-01-15 15:35:44 +0800

    login&register

--- app/pages/mine/childrenpage/help/helppage.js
+++ app/pages/mine/childrenpage/help/helppage.js
@@ -9,33 +9,68 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import gql from "graphql-tag";
 
-const query = gql`
-query{
-  CarInfoList{
-    content{
-      id
-      model
-    }
-  }
-}
-`;
 
-const GetCarInfoList = () => (<Query
-  query={query}>{
-    ({ loading, error, data }) => {
-      if (loading == true) return <View><Text>正在加载</Text></View>;
-      console.log(data.CarInfoList.content)
-      return <View><Text>成功</Text></View>
-    }
+
+// const query = gql`
+// query{
+//   CarInfoList{
+//     content{
+//       id
+//       model
+//     }
+//   }
+// }
+// `;
+
+// const GetCarInfoList = () => (<Query
+//   query={query}>{
+//     ({ loading, error, data }) => {
+//       if (loading == true) return <View><Text>正在加载</Text></View>;
+//       console.log(data.CarInfoList.content)
+//       return <View><Text>成功</Text></View>
+//     }
+//   }
+// </Query>);
+
+function loginn() {
+  let formData = {
+    "name":'xzc',
+    "password":'123'
   }
-</Query>);
+
+  fetch('http://192.168.0.112:8080/login',
+  {
+     method:"POST",   //请求方法
+     mode: "cors",
+     body:JSON.stringify(formData),   //请求体
+　　　　headers: {
+　　　　'Accept': 'application/json',
+　　　　'Content-Type': 'application/json',
+　　　　 }})
+      .then((response) => {
+        res=JSON.parse(response._bodyText)
+        console.log(res)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+}
+
 export default class help extends Component {
   render() {
     return (
       <View>
-
-        <GetCarInfoList />
+      <Button
+        style={{fontSize: 20, color: 'red'}}
+        styleDisabled={{color: 'red'}}
+        onPress={() => loginn()}>
+        点击
+      </Button>
+        
       </View>
     );
   }
+
+  
 }
+
